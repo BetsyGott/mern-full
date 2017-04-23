4c51786ea00ec5ed642b407ef7b6a8cb8538e8c1
Betsy Gottesman <eightamps@gmail.com>
2017-04-23 13:12:35 -0700

    commit before merge

--- client/index.js
+++ client/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './App';
-import {browserHistory, Router, Route} from 'react-router';
+import {browserHistory} from 'react-router';
 const mountApp = document.getElementById('root');
-console.log("My key = " + process.env.GAPI_KEY);
 export const GAPI_KEY = process.env.GAPI_KEY;
 import routes from './routes';
 
